3a2fb0ac986150a70a68e408790d094fdd9503f4
Andrew <32670729+mazalkov@users.noreply.github.com>
2021-03-03 10:57:39 +0000

    Create Lists.py

---
+++ LearnPython/Learn the Basics/Lists.py
@@ -0,0 +1,18 @@
+# https://www.learnpython.org/en/Lists
+
+
+numbers = []
+strings = []
+names = ["John", "Eric", "Jessica"]
+
+# write your code here
+second_name = names[1]
+
+numbers.append([1, 2, 3])
+strings.append(['hello', 'world'])
+
+
+# this code should write out the filled arrays and the second name in the names list (Eric).
+print(numbers)
+print(strings)
+print("The second name on the names list is %s" % second_name)
