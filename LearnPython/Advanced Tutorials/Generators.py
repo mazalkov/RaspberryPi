2045f0453cda4b44a524dfd9b505af9c1c3b8709
Andrew <32670729+mazalkov@users.noreply.github.com>
2021-03-17 11:27:32 +0000

    Create Generators.py

---
+++ LearnPython/Advanced Tutorials/Generators.py
@@ -0,0 +1,22 @@
+# https://www.learnpython.org/en/Generators
+
+
+# fill in this function
+def fib():
+    a, b = 1, 1
+    
+    while True:
+        yield a
+        a, b = b, b+a
+
+# testing code
+import types
+if type(fib()) == types.GeneratorType:
+    print("Good, The fib function is a generator.")
+
+    counter = 0
+    for n in fib():
+        print(n)
+        counter += 1
+        if counter == 10:
+            break
